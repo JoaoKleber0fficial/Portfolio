470ba108d11f073e40db0f2f7c90865856c19b2a
João Kleber <134006422+JoaoKleber0fficial@users.noreply.github.com>
2024-09-02 10:07:06 -0300

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,45 @@
+let showDetail = false;
+let closing = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const avancaButton = document.getElementById('avanca');
+    const voltarButton = document.getElementById('voltar');
+    const carousel = document.querySelector('.carousel2'); 
+    const listHTML = document.querySelector('.carousel2 .list'); 
+    const backButton = document.getElementById('back');
+
+    const showSlider = (type) => {
+        if (!avancaButton || !voltarButton || !carousel || !listHTML) return;
+
+        avancaButton.style.pointerEvents = 'none';
+        voltarButton.style.pointerEvents = 'none';
+
+        carousel.classList.remove('avanca', 'voltar');
+        let items = document.querySelectorAll('.carousel2 .list .item');
+        if (type === 'avanca') {
+            listHTML.appendChild(items[0]);
+            carousel.classList.add('avanca');
+        } else {
+            listHTML.prepend(items[items.length - 1]);
+            carousel.classList.add('voltar');
+        }
+        setTimeout(() => {
+            avancaButton.style.pointerEvents = 'auto';
+            voltarButton.style.pointerEvents = 'auto';
+        }, 500);
+    }
+
+    if (avancaButton && voltarButton) {
+        avancaButton.onclick = () => showSlider('avanca');
+        voltarButton.onclick = () => showSlider('voltar');
+    }
+
+    if (backButton) {
+        backButton.onclick = () => {
+            carousel.classList.remove('showDetail');
+        }
+    }
+    
+});
+
+
